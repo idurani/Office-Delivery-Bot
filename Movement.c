ec4da73184f132ef84e3d78537925e785692f6cb
idurani <44308944+idurani@users.noreply.github.com>
2018-11-29 19:19:52 -0600

    Add files via upload

--- Movement.c
+++ Movement.c
@@ -10,6 +10,22 @@
 #include "math.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <inits_tiva.h>
+#include "lcd.h"
+#include "timer.h"
+#include "uart.h"
+
+
+
+#define tpd 153
+#define zeroPos 7500
+#define power -1.086
+#define coe 65430
+
+
+
+volatile float sweepData[2][91];
+
 
 
 void move_forward(oi_t *sensor, int centimeters);
@@ -19,10 +35,29 @@ void turn_left(oi_t *sensor);
 void turn_right(oi_t *sensor);
 void spin(oi_t *sensor);
 void bumper_backward(oi_t *sensor, int millimeters);
+void read_turret();
+void sweep_servo();
+void checkObjects();
 
 
 int leftWheelSpeed = 250;
 int rightWheelSpeed = 250;
+
+/**
+ * width is the width of the object
+ * center is the degree messurement
+ * distance is the distance form the roomba
+ */
+struct block
+{
+    float width;
+    int center;
+    float distance;
+
+};
+
+volatile struct block finds[6];
+volatile int objectCount = 0;
 
 typedef enum{
     Forward,
@@ -44,16 +79,32 @@ typedef enum{
     leftBump,
     rightBump,
     forward,
+    objectCenter,
+    objectRight,
 }state;
 
 state s = forward;
+
+
+
+
+
 
 int main(void)
 {
    oi_t *sensor = oi_alloc();
    oi_init(sensor);
 
+   ping_init();
+   adc_init();
+   PWM_init();
+
+
    while(1){
+       if(s == forward){
+         read_turret();
+         checkObjects();
+       }
 
        //scan
        // case for object in front
@@ -87,15 +138,21 @@ int main(void)
            turn_left(sensor);
            s = forward;
        }
+       else if(s == objectCenter){
+           turn(sensor, -45);
+           move_forward(sensor, 20);
+           turn(sensor, 45);
+           s = forward;
+       }
+       else if(s == objectRight){
+           move_backward(sensor, 10);
+           turn(sensor, -90);
+           move_forward(sensor, 20);
+           turn(sensor, 90);
+           s = forward;
+       }
 
-        //    move_forward(sensor_data, 20);
-        //    turn_right(sensor_data);
-        //    turn_right(sensor_data);
-        //    turn_right(sensor_data);
-        //    turn_left(sensor_data);
-        //    move_forward(sensor_data, 20);
-        //    move_backward(sensor_data, 20);
-        //    spin(sensor_data);
+
    }
 	return 0;
 
@@ -103,7 +160,36 @@ int main(void)
 
 }
 
+void checkObjects(){
+    int i;
+    //says if there is an object to the right
+    int objectRightFlag = 0;
+    //distance we are comparing to
+    float D;
 
+    for(i = 0; i < 6; i++){
+        D = 20/(cos(finds[i].center-90));
+        if(finds[i].distance < 35 && finds[i].center > 0){
+            if(finds[i].center < 54){
+                objectRightFlag = 1;
+            }
+            if((finds[i].center > 54) && (finds[i].center < 128)){
+                if(s == forward){
+                  s = objectCenter;
+                }
+            }
+
+            //this mmight be the problem
+            if(D >= finds[i].distance){
+                s = objectCenter;
+            }
+        }
+    }
+
+    if(s == objectCenter && objectRightFlag == 1){
+        s = objectRight;
+    }
+}
 
 
 // function to have the cybot move forward
@@ -333,6 +419,92 @@ void bumper_backward(oi_t *sensor, int millimeters){
     default:
         break;
     }
+}
+
+
+
+
+
+
+
+void sweep_servo(){
+    //char str[50];
+    //sprintf(str, "Deg\tIR Distance(cm)\tSonarDistance(cm)\n\r");
+    int i;
+    //sweep the servo across 180 degrees, 2 degrees at a time
+    for(i = 0; i < 91; i += 1){
+        //move by two degrees
+        move_servo(i*2);
+        //give the servo time to move
+        timer_waitMillis(20);
+        //read ir data
+        read_ir();
+        //save data into the next section of the array
+        sweepData[0][i] = get_dis_ir();
+        sweepData[1][i] = read_ping();
+        //sprintf(str, "%d\t%.1f\t\t%.1f\n\r", i*2, sweepData[0][i], sweepData[1][i] );
+        //uart_sendStr(str);
+    }
+}
+
+
+
+void read_turret(){
+    //record data
+        sweep_servo();
+    //variables for iterating through data to find blocks and info about blocks
+        objectCount = 0;
+        int i;
+        int looking = 0;
+        int currentWidth = 0;
+        float minDist = 100;
+
+        for(i = 0; i < 6; i++){
+            //if the center is -1 then the object is NULL
+            finds[i].center = -1;
+        }
+        //start iterating through data gathered
+        for(i = 0; i < 90; i++)
+        {
+            //if it finds an object, set the boolean variable indicating an object found to 1
+            if(sweepData[0][i] < 100 && sweepData[1][i] < 50 && looking == 0)
+            {
+                looking = 1;
+            }
+            //if the end of an object is found
+            if((sweepData[0][i] >= 100 || sweepData[1][i] >= 50) && looking == 1)
+            {
+                //set boolean variable indicating an object found to 0
+                looking = 0;
+                //record distance
+                finds[objectCount].distance = minDist;
+                //compute center of object
+                finds[objectCount].center = i*2-currentWidth;
+                //compute width of object using arc length formula
+                finds[objectCount].width = currentWidth * (6.28 / 180) * minDist;
+                //iterate to the next block in the block array
+                objectCount++;
+                //reset helper variables
+                currentWidth = 0;
+                minDist = 100;
+            }
+            //if currently looking at a block
+            if(looking == 1)
+            {
+                //increase the width helper variable
+                currentWidth++;
+                //make sure the minimum distance of the object is still the minimum distance
+                if(sweepData[1][i] < minDist)
+                {
+                    minDist = sweepData[1][i];
+                }
+            }
+            //if the block array is full
+            if(objectCount == 6)
+            {
+                break;
+            }
+        }
 }
 
 
