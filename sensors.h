7c626caa73272a536c6d3ac755cf01140fe0bd01
Taylor <igneous.star@gmail.com>
2018-12-06 13:56:08 -0600

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -1,7 +1,7 @@
 /**
  * sensors.h
  * @brief This header file will contain the functions pertaining to the sensors
- * @author Jared Rickard
+ * @author Jared Rickard, Taylor Burton
  * @date 12/05/2018
  */
 
